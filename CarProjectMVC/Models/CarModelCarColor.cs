ef1e49afd4ec966eba496fb86b77cc3b4166dff9
c.sharp.proger <c.sharp.proger@outlook.com>
2023-10-17 16:42:10 +0300

    Коммит с саммари для CarModelCarColor

---
+++ CarProjectMVC/Models/CarModelCarColor.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarProjectMVC.Models
+{
+    /// <summary>
+    /// Таблица для связи многие ко многим между моделями и цветами
+    /// </summary>
+    public class CarModelCarColor
+    {
+        /// <summary>
+        /// ID связи
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// ID модели
+        /// </summary>
+        public int ModelId { get; set; }
+
+        /// <summary>
+        /// ID цвета
+        /// </summary>
+        public int ColorId { get; set; }
+    }
+}
